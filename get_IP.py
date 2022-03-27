00e10b0fc95133ffb0a22ab442879ffbb95f2b26
Sayantan Mandal <83362170+SayantanMandal2000@users.noreply.github.com>
2022-03-27 12:32:45 +0530

    Delete get_IP.py

--- get_IP.py
+++
@@ -1,9 +0,0 @@
-import socket
-host_name=socket.gethostname()
-ip1=socket.gethostbyname(host_name)
-print(host_name)
-print("IP Address is::",ip1)
-remote_host='www.jaduniv.edu.in'
-print(remote_host)
-ip2=socket.gethostbyname(remote_host)
-print("Remote IP Address::",ip2)
